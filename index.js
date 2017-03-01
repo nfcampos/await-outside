00f8bcf6cdc63c0db35fdbddc6e563f6330e3714
Nuno Campos <nuno.campos@me.com>
2017-03-01 23:10:40 +0000

    Improvements to context and stack traces - code is now executed in the correct context - better implementation of isRecoverableError - format stack trace to match regular behaviour

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const repl = require('repl')
 const vm = require('vm')
 
+// see https://github.com/nodejs/node/blob/master/lib/repl.js#L1371
+function isRecoverableError(e) {
+  if (e && e.name === 'SyntaxError') {
+    var message = e.message;
+    if (message === 'Unterminated template literal' ||
+        message === 'Missing } in template expression') {
+      return true;
+    }
+
+    if (message.startsWith('Unexpected end of input') ||
+        message.startsWith('missing ) after argument list') ||
+        message.startsWith('Unexpected token'))
+      return true;
+
+  }
+  return false;
+}
+
+function formatError(e) {
+  const stackLines = e.stack.split('\n')
+
+  // remove async function invocation from stack
+  // ie. first line from `repl` file starting from bottom
+  const i = stackLines.reverse().findIndex(l => l.trim().startsWith('at repl:'))
+  if (i) stackLines.splice(i, 1)
+
+  e.stack = stackLines.reverse().join('\n')
+  return e
+}
+
 function addAwaitOutside(replServer) {
   /*
   - allow whitespace before everything else
@@ -13,17 +43,12 @@ function addAwaitOutside(replServer) {
   let re = /^\s*(?:([a-zA-Z_$][0-9a-zA-Z_$]*)\s*=\s*)?(await[\s\S]*)/
 
   const wrap = (code, binder) => {
-    return `(async function() {
-      let result = (${code.trim()});
+    // strange indentation keeps column offset correct in stack traces
+    return `(async function() { let result = (
+${code.trim()}
+);
       ${binder ? `global.${binder} = result` : 'return result'}
     }())`
-  }
-
-  const isRecoverableError = (error) => {
-    if (error.name === 'SyntaxError') {
-      return /^(Unexpected end of input|Unexpected token)/.test(error.message)
-    }
-    return false
   }
 
   replServer.eval = function(originalEval) {
@@ -35,12 +60,16 @@ function addAwaitOutside(replServer) {
       }
 
       const code = wrap(match[2], match[1])
+
       try {
-        vm.runInContext(code, vm.createContext(context))
-          .then(r => callback(null, r), callback)
+        var script = vm.createScript(code, { filename, displayErrors: true, lineOffset: -1 })
       } catch (e) {
         callback(isRecoverableError(e) ? new repl.Recoverable(e) : e)
+        return
       }
+
+      script.runInThisContext({ displayErrors: true, breakOnSigint: true })
+        .then(r => callback(null, r), err => callback(formatError(err)))
     }
   }(replServer.eval)
 }
