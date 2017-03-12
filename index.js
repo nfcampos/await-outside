db89a6fb54eef7dd8bc37717b87295b1ca05e548
Nuno Campos <nuno.campos@me.com>
2017-03-12 16:51:54 +0000

    Add prettier

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const repl = require('repl')
-const vm = require('vm')
+const repl = require("repl");
+const vm = require("vm");
 
 // see https://github.com/nodejs/node/blob/master/lib/repl.js#L1371
 function isRecoverableError(error) {
-  if (error && error.name === 'SyntaxError') {
+  if (error && error.name === "SyntaxError") {
     var message = error.message;
-    if (message === 'Unterminated template literal' ||
-        message === 'Missing } in template expression') {
+    if (
+      message === "Unterminated template literal" ||
+      message === "Missing } in template expression"
+    ) {
       return true;
     }
 
-    if (message.startsWith('Unexpected end of input') ||
-        message.startsWith('missing ) after argument list') ||
-        message.startsWith('Unexpected token'))
+    if (
+      message.startsWith("Unexpected end of input") ||
+      message.startsWith("missing ) after argument list") ||
+      message.startsWith("Unexpected token")
+    )
       return true;
-
   }
   return false;
 }
 
 function formatError(error) {
-  const stackLines = error.stack.split('\n')
+  const stackLines = error.stack.split("\n");
 
   // remove async function invocation from stack
   // ie. first line from `repl` file starting from bottom
-  const i = stackLines.reverse().findIndex(l => l.trim().startsWith('at repl:'))
-  if (i !== -1) stackLines.splice(i, 1)
+  const i = stackLines
+    .reverse()
+    .findIndex(l => l.trim().startsWith("at repl:"));
+  if (i !== -1) stackLines.splice(i, 1);
 
-  error.stack = stackLines.reverse().join('\n')
-  return error
+  error.stack = stackLines.reverse().join("\n");
+  return error;
 }
 
 /*
@@ -39,52 +44,53 @@ function formatError(error) {
   - this is overly restrictive but is easier to maintain
 - capture `await <anything that follows it>`
 */
-let re = /^\s*(?:([a-zA-Z_$][0-9a-zA-Z_$]*)\s*=\s*)?(\(?\s*await[\s\S]*)/
+let re = /^\s*(?:([a-zA-Z_$][0-9a-zA-Z_$]*)\s*=\s*)?(\(?\s*await[\s\S]*)/;
 
 function isAwaitOutside(source) {
-  return re.test(source)
+  return re.test(source);
 }
 
 function wrapAwaitOutside(source) {
-  const [_, identifier, expression] = source.match(re)
+  const [_, identifier, expression] = source.match(re);
 
   // strange indentation keeps column offset correct in stack traces
   return `(async function() { try { let result = (
 ${expression.trim()}
 );
-${identifier ? `global.${identifier} = result` : 'return result'}
+${identifier ? `global.${identifier} = result` : "return result"}
 } catch(error) {
 global.ERROR = error
 throw error
 }
-  }())`
+  }())`;
 }
 
 function addAwaitOutsideToReplServer(replServer) {
-  replServer.eval = function(originalEval) {
-    return function (source, context, filename, cb) {
+  replServer.eval = (function(originalEval) {
+    return function(source, context, filename, cb) {
       if (!isAwaitOutside(source)) {
-        return originalEval.call(this, source, context, filename, cb)
+        return originalEval.call(this, source, context, filename, cb);
       }
 
-      const newSource = wrapAwaitOutside(source)
-      const options = { filename, displayErrors: true, lineOffset: -1 }
+      const newSource = wrapAwaitOutside(source);
+      const options = { filename, displayErrors: true, lineOffset: -1 };
 
       try {
-        var script = vm.createScript(newSource, options)
+        var script = vm.createScript(newSource, options);
       } catch (e) {
-        cb(isRecoverableError(e) ? new repl.Recoverable(e) : e)
-        return
+        cb(isRecoverableError(e) ? new repl.Recoverable(e) : e);
+        return;
       }
 
-      script.runInThisContext({ displayErrors: true, breakOnSigint: true })
-        .then(r => cb(null, r), err => cb(formatError(err)))
-    }
-  }(replServer.eval)
+      script
+        .runInThisContext({ displayErrors: true, breakOnSigint: true })
+        .then(r => cb(null, r), err => cb(formatError(err)));
+    };
+  })(replServer.eval);
 }
 
 module.exports = {
   addAwaitOutsideToReplServer,
   wrapAwaitOutside,
-  isAwaitOutside,
-}
+  isAwaitOutside
+};
--- repl.js
+++ repl.js
@@ -1,5 +1,5 @@
-const { addAwaitOutsideToReplServer } = require('.')
+const { addAwaitOutsideToReplServer } = require(".");
 
 // if used as `node --require await-outside/repl` repl server will only be
 // created after this file is evaluated, thus the setImmediate to wait for it
-setImmediate(() => addAwaitOutsideToReplServer(global.repl.repl))
+setImmediate(() => addAwaitOutsideToReplServer(global.repl.repl));
